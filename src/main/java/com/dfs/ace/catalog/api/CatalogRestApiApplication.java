edcacb8df40bb365d00d7526832ba5b4ab88621f
eugene <eugene.goldberg117@gmail.com>
2018-01-06 14:46:13 -0600

    first commit

---
+++ src/main/java/com/dfs/ace/catalog/api/CatalogRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.dfs.ace.catalog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogRestApiApplication.class, args);
+	}
+}
